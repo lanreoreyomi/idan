aefe22fd9c7482ca06b6367a7ad6932f6d93734e
lanre <lanre1012@yahoo.com>
2025-03-13 23:22:59 -0400

    Created User, Notification, Gig, Payment, Review, Notification, Discovery microservices

---
+++ notification/src/test/java/com/idan/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idan.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ review/src/main/java/com/idan/review/ReviewApplication.java
@@ -0,0 +1,13 @@
+package com.idan.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+}
