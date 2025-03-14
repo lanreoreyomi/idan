04b6d16260000ae037e02af60eae5dd093849fbb
lanre <lanre1012@yahoo.com>
2025-03-13 23:03:30 -0400

    idan

---
+++ src/main/java/com/idan/IdanApplication.java
@@ -0,0 +1,13 @@
+package com.idan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idan/IdanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
